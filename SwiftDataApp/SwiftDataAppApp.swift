df23d25e2dc8518735a9de7d6b2b26cd3c888873
Armanul Momin <armanul.momin@gmail.com>
2025-09-16 23:34:00 +1000

    Initial Commit

---
+++ SwiftDataApp/SwiftDataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataAppApp.swift
+//  SwiftDataApp
+//
+//  Created by Arman on 16/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
